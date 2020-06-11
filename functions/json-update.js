5503c3c6db588c20dc95e2e9d5551c415e618804
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-06-12 01:34:42 +0200

    Update json-update.js

--- functions/json-update.js
+++ functions/json-update.js
@@ -1,7 +1,7 @@
 const editJsonFile = require('edit-json-file');
 
 exports.handler = function(event, context, callback) {
-	let file = editJsonFile(process.cwd() + '/src/tmp/data.json');
+	let file = editJsonFile(path.join(__dirname, '..', 'tmp'));
 	file.set('test', 20);
 	file.save();
 
