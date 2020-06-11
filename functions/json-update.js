b28767a13c19eff3b1a6015082229c9360156b97
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-06-11 23:37:48 +0200

    Update json-update.js

--- functions/json-update.js
+++ functions/json-update.js
@@ -1,6 +1,7 @@
+import editJsonFile from 'edit-json-file';
+
 exports.handler = (event, context, callback) => {
-	callback(null, {
-		statusCode: 200,
-		body: 'Hello, World',
-	});
+	let file = editJsonFile('/public/static/data.json');
+	file.set('test', 20);
+	file.save();
 };
