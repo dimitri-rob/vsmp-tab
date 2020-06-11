b2741ac048af01695cd28586ac8d96a9670505b7
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-06-11 23:52:31 +0200

    path

--- src/components/vsmp.vue
+++ src/components/vsmp.vue
@@ -18,7 +18,7 @@
 import json from '../../public/static/data';
 
 function updateJson() {
-	return fetch('../json-update.js').then((response) => {
+	return fetch('../../public/static/json-update.js').then((response) => {
 		return response;
 	});
 }
