1a4a4253adf330442b08e52600cdce1543cad93a
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-06-12 00:56:55 +0200

    fix path

--- functions/json-update.js
+++ functions/json-update.js
@@ -1,7 +1,7 @@
 const editJsonFile = require('edit-json-file');
 
 exports.handler = function(event, context, callback) {
-	let file = editJsonFile(process.cwd() + 'tmp/data.json');
+	let file = editJsonFile(process.cwd() + '/tmp/data.json');
 	file.set('test', 20);
 	file.save();
 
