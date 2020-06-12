a6e785b03108fe7f302edebdcb44743601b7b6af
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-06-12 12:10:52 +0200

    Update json-update.js

--- functions/json-update.js
+++ functions/json-update.js
@@ -3,11 +3,11 @@ const editJsonFile = require('edit-json-file');
 
 exports.handler = function(event, context, callback) {
 	let file = editJsonFile(path.join(__dirname, 'data.json'));
-	file.set('test', 20);
-	file.save();
+	//file.set('test', 20);
+	//file.save();
 
 	callback(null, {
 		statusCode: 200,
-		body: 'Hello, World',
+		body: file,
 	});
 };
