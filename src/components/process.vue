be286f5e73182bea4b0ee40e19bf04dce0567954
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-06-14 02:28:02 +0200

    Suppression process

--- src/components/process.vue
+++ src/components/process.vue
@@ -5,17 +5,5 @@
 </template>
 
 <script>
-import ffmpeg from 'easy-ffmpeg';
-import pathToFfmpeg from 'ffmpeg-static';
-import extractFrames from 'ffmpeg-extract-frames';
-
-export default {
-	mounted() {
-		extractFrames({
-			input: '/media/1.mp4',
-			output: '/media/frames/frame-%d.png',
-			offsets: [1000, 2000, 10000],
-		});
-	},
-};
+export default {};
 </script>
