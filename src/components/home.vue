8cf1d1d77259150465a6d6a05042b54a525ea7dc
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-06-15 13:49:54 +0200

    Add firefox plugin mention on homepage

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,7 +2,7 @@
 	<div class="home">
 		<section class="home__wrap">
 			<header class="home__head">
-				<h1 class="home__title">VSMP TAB</h1>
+				<h1 class="home__title">VSMP Tab</h1>
 				<h2 class="home__subtitle">
 					a movie played very slooooooooooooooowly
 					<br />in your browser tab
@@ -12,20 +12,35 @@
 			<main class="home__content">
 				<div class="home__intro">
 					<div class="home__excerpt">
-						Based on
-						<a
-							href="https://medium.com/s/story/very-slow-movie-player-499f76c48b62"
-							target="_blank"
-						>Bryan Boyer’s idea</a>, get a new frame from a movie
-						each hour when you open a new tab in your browser. It is
-						safe for work.
+						<p>
+							A new frame from a movie is displayed every hour in
+							your browser.
+						</p>
+						<p>
+							Use the
+							<a href="https://addons.mozilla.org/addon/vsmp-tab/" target="_blank">Firefox plugin</a> and display current frame each time you
+							open a new window or tab. It is safe
+							for work.
+						</p>
+						<p>
+							<small>
+								Based on
+								<a
+									href="https://medium.com/s/story/very-slow-movie-player-499f76c48b62"
+									target="_blank"
+								>Bryan Boyer’s idea</a>
+							</small>
+						</p>
 					</div>
 
 					<div class="home__cta">
-						<div>
-							<router-link :to="{ name: 'Watch' }" class="home__cta__btn">watch movie</router-link>
-							<span class="home__cta__txt">and add it as start page</span>
-						</div>
+						<router-link :to="{ name: 'Watch' }" class="home__cta__btn">watch movie</router-link>
+						<span class="home__cta__txt">
+							<a href="https://addons.mozilla.org/addon/vsmp-tab/" target="_blank">
+								or use
+								<br />Firefox plugin
+							</a>
+						</span>
 					</div>
 				</div>
 
